71f73c0c28551d61d0216753d719388047ba7cbe
kaisho1738 <orichimaru619@gmail.com>
2026-08-24 13:34:47 +0800

    refactor: move styles.css to proper directory

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import { ThemeProvider } from './context/ThemeContext';
 import { AuthProvider } from './context/AuthContext';
+import './styles.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
